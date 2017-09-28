5782f920688b48c83a24b6091658535713bdbaa2
techmsi <sophia.idro@gmail.com>
2017-09-28 10:10:16 -0400

    Add modules

--- index.js
+++ index.js
@@ -1,52 +1,47 @@
-var express = require('express');
-var app = express();
-var url = require('url');
-var request = require('request');
+// Load Environment Variables
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const request = require('request');
 
-var format = ".json";
-var apikey = process.env.WU_ACCESS  //WU API key; will be set in Heroku
+const apikey = process.env.WU_ACCESS; // WU API key; will be set in Heroku
 
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//use port is set in the environment variable, or 9001 if it isn’t set.
+// use port is set in the environment variable, or 9001 if it isn’t set.
 app.set('port', (process.env.PORT || 9001));
 
-//for testing that the app is running
-app.get('/', function(req, res){
+// for testing that the app is running
+app.get('/', function (req, res) {
   res.send('Running!!');
 });
 
-//app.post is triggered when a POST request is sent to the URL ‘/post’
-app.post('/post', function(req, res){
-  //take a message from Slack slash command
-  var query = req.body.text;
+// app.post is triggered when a POST request is sent to the URL ‘/post’
+app.post('/post', function (req, res) {
+  // take a message from Slack slash command
+  console.log("req.body.text ", req.body.text);
   var cityName = req.body.text || 'New York';
   var ENDPOINT = `http://api.openweathermap.org/data/2.5/weather?appid=${apikey}&q=${cityName}`;
 
-  var parsed_url = url.format({
-    pathname: ENDPOINT,
-  });
+  request(ENDPOINT, function (error, response, body) {
+    console.log("Going to ENDPOINT ", ENDPOINT);
+    
+    if (!error && response.statusCode === 200) {
+      const data = JSON.parse(body);
+      const { name: location, weather: { main: weatherCondition, icon }, main: { temp: temperature } } = data;
 
-  console.log(parsed_url);
-
-  request(parsed_url, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var data = JSON.parse(body);
-      var temperature = data.current_observation.temperature_string;
-      var weatherCondition = data.current_observation.weather
-      var icon_url = data.current_observation.icon_url
-      var location = data.current_observation.display_location.full
-
-      var body = {
-        response_type: "in_channel",
-        "attachments": [
+      const body = {
+        response_type: 'in_channel',
+        attachments: [
           {
-            "text": "Location: " + location + "\n"
-                  + "Temperature: " + temperature + "\n"
-                  + "Condition: " + weatherCondition,
-            "image_url": icon_url,
+            'text': `
+              Location: ${location}
+              Temperature: ${temperature}
+              Condition: ${weatherCondition}
+              `,
+            'image_url': icon
           }
         ]
       };
@@ -55,7 +50,7 @@ app.post('/post', function(req, res){
   });
 });
 
-//tells Node which port to listen on
-app.listen(app.get('port'), function() {
+// tells Node which port to listen on
+app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'));
 });
